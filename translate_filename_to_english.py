e4e5dc40285636a2794eb2d0ba94b49f379e985c
Ilya Lotnik <golkiper-leo@yandex.ru>
2021-02-14 22:42:24 +0300

    add docstring

--- translate_filename_to_english.py
+++ translate_filename_to_english.py
@@ -4,12 +4,25 @@ from googletrans import Translator
 
 
 def google_translate(message):
+    """
+
+    Переводит входящую строку с русского на английский
+
+    :param message: Строка на русском языке
+    :return: Строка на английском языке
+    """
     translator = Translator()
     translations = translator.translate(message, dest="en")
     return translations.text
 
 
 def is_russian(input_string):
+    """
+    Проверяет, состоит ли входящее слово из символов киррилицы
+
+    :param input_string: Строка
+    :return: Если символы киррилицы присутствуют в строке, возвращает True
+    """
     russian_abc = [
         "а",
         "б",
@@ -52,6 +65,13 @@ def is_russian(input_string):
 
 
 def delete_unwanted_characters(input_string):
+    """
+    Удаляет из строки нежелательные символы.
+
+    :param input_string: Входящая строка, может содержать различные
+    недопустимые символы
+    :return: Новая строка new_string, не содержит недопустимых символов
+    """
     unwanted_characters = [
         "!",
         "@",
@@ -78,6 +98,13 @@ def delete_unwanted_characters(input_string):
 
 
 def file_rename(new_filenames_dict, directory):
+    """
+    Переименовывает файлы в папке назначения
+
+    :param new_filenames_dict: Словарь со значениями вида
+    {Старое имя файла на русском: Новое_имя_файла_на_английском}
+    :param directory: путь к папке, где происходит переименование файлов
+    """
 
     for key, values in new_filenames_dict.items():
         old_filename = key
@@ -90,6 +117,13 @@ def file_rename(new_filenames_dict, directory):
 
 
 def get_extension(filename):
+    """
+    Возвращает расширение входного файла.
+
+    :param filename: Входящий файла вида Имя файла.txt
+    :return: возвращает расширение входящего файла - txt
+    """
+
     extension = ""
     for char in filename[::-1]:
         if char != ".":
@@ -99,6 +133,14 @@ def get_extension(filename):
 
 
 def system_path_validator(path):
+    """
+    Проверяет, существует ли такой путь(path) в системе.
+
+    :param path: Путь, где будет происходить переименование файлов
+    :return: Путь, точно существующий в системе, где будет происходить
+    переименование файлов
+    """
+
     path_exists = os.path.exists(path)
 
     while path_exists is False:
@@ -110,6 +152,11 @@ def system_path_validator(path):
 
 
 def answer_validator():
+    """
+    Валидатор правильности ответа на вопрос
+
+    :return: False или True, в зависимости от введенного значения
+    """
     while True:
         answer = input()
         if answer == "n":
@@ -122,6 +169,15 @@ def answer_validator():
 
 
 def translate_example(input_path):
+    """
+    Переименовыавает файлы, содержащиеся в конкретной папке, добавляет
+    результат перевода в словарь
+
+    :param input_path: Путь к папке в системе, где будет осуществляться
+    перевод файлов, которые содержит папка
+    :return: Словарь со значениями вида
+    {Старое имя файла на русском: Новое_имя_файла_на_английском}
+    """
     files = os.listdir(input_path)
 
     filenames_dict = {}
